b66a5bb9a8b3497d9cfd0cb8ac3ffd26310a95b1
Renan <renan.specicaste@gmail.com>
2022-12-20 13:32:19 +0100

    feat: added tests

---
+++ src/test/java/com/wolobi/mavenproject1/TestMavenproject1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.wolobi.mavenproject1;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author speci
+ */
+public class TestMavenproject1 {
+    
+    @Test
+    public void justAnExample() {
+        Assert.assertTrue(3 > 2);
+    }
+}
